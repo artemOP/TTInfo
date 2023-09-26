6f99816eb7d27cd6a022a5dc2206c41c5835f938
Matthew <matthew.brough@hotmail.com>
2023-09-26 09:19:11 +0100

    server type

--- ttinfo/database/schema.sql
+++ ttinfo/database/schema.sql
@@ -1,2 +1,8 @@
-CREATE TABLE IF NOT EXISTS keys(vrp_id INT PRIMARY KEY, private TEXT, public TEXT);
+DO $$ BEGIN
+    CREATE TYPE SERVER AS ENUM('main', 'beta');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE IF NOT EXISTS keys(vrp_id INT, server SERVER, private TEXT, public TEXT, PRIMARY KEY(vrp_id, server));
 CREATE TABLE IF NOT EXISTS snowflake2user(vrp_id INT PRIMARY KEY, snowflake BIGINT NOT NULL);
