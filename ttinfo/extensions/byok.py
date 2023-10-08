4898c13c1ca97ae2a0fd6fa3d22212832a463f02
Matthew <matthew.brough@hotmail.com>
2023-10-08 22:10:14 +0100

    bugfixes

--- ttinfo/extensions/byok.py
+++ ttinfo/extensions/byok.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal, Optional
 
 import discord
 from discord import app_commands
@@ -9,8 +9,6 @@ from discord.ext import commands
 from ..http.enums import Server
 
 if TYPE_CHECKING:
-    from typing import Literal, Optional
-
     from discord import Interaction
 
     from ttinfo.core.bot import Bot
@@ -30,33 +28,32 @@ class Byok(commands.GroupCog, name="byok"):
         self.logger.info(f"{self.qualified_name} cog unloaded")
 
     @app_commands.command(name="add")
-    @app_commands.rename(private="Private Key", public="Public Key")
     async def add_keys(
         self,
         interaction: Interaction,
-        private: Optional[str] = None,
-        public: Optional[str] = None,
+        private_key: Optional[str] = None,
+        public_key: Optional[str] = None,
         server: Server = Server.main,
     ):
         """Add your keys to the bot
 
         Args:
             interaction (Interaction): Internal
-            private (Optional[str]): Your private API key
-            public (Optional[str]): Your public API key
+            private_key (Optional[str]): Your private API key
+            public_key (Optional[str]): Your public API key
             server (Optional[Server]): The server the keys are tied to
         """
         vrp_id = await self.bot.client.fetch_vrp(
             interaction.user.id,
             server,
-            key=private or self.bot.client.fallback_key,
+            key=private_key or self.bot.client.fallback_key,
         )
         await self.bot.pool.execute(
-            "INSERT INTO keys(vrp_id, server, private, public) VALUES($1, $2, $3, $4) ON CONFLICT DO UPDATE SET public=EXCLUDED.public WHERE EXCLUDED.public != null, private=EXCLUDED.private WHERE EXCLUDED.private != null",
+            "INSERT INTO keys(vrp_id, server, private, public) VALUES($1, $2, $3, $4) ON CONFLICT(vrp_id, server) DO UPDATE SET public=coalesce(excluded.public, keys.public), private=coalesce(excluded.private, keys.private)",
             vrp_id.user_id,
             server.name,
-            private,
-            public,
+            private_key,
+            public_key,
         )
         return await interaction.response.send_message(f"Keys updated", ephemeral=True)
 
@@ -75,7 +72,7 @@ class Byok(commands.GroupCog, name="byok"):
             server (Optional[Server]): The server the keys are tied to
         """
         vrp_id = await self.bot.client.fetch_vrp(interaction.user.id, server)
-        await self.bot.pool.execute("UPDATE keys SET {key}=Null WHERE vrp_id=$1", vrp_id.user_id)
+        await self.bot.pool.execute(f"UPDATE keys SET {key}=Null WHERE vrp_id=$1", vrp_id.user_id)
         await self.bot.client.get_keys(vrp_id.user_id, server, True)
         return await interaction.response.send_message(f"{key} key removed", ephemeral=True)
 
@@ -88,11 +85,19 @@ class Byok(commands.GroupCog, name="byok"):
             server (Server, optional): The server the keys are tied to
         """
         await interaction.response.defer(ephemeral=True)
-        key = self.bot.pool.fetchrow(
-            "SELECT K.private FROM keys K LEFT JOIN snowflake2user S ON S.vrp_id=K.vrp_id WHERE S.snowflake=$1",
+        key = await self.bot.pool.fetchrow(
+            "SELECT K.private FROM keys K LEFT JOIN snowflake2user S ON S.vrp_id=K.vrp_id WHERE S.snowflake=$1 AND K.server=$2",
             interaction.user.id,
+            server.name,
         )
-        await interaction.followup.send(embed=discord.Embed(title="Charges remaining", description=key), ephemeral=True)
+        if not key:
+            return await interaction.followup.send(
+                "You have not added a private key yet", ephemeral=True
+            )  # todo: Command mention
+        charges = await self.bot.client.fetch_charges(key["private"], server, force=True)
+        await interaction.followup.send(
+            embed=discord.Embed(title="Charges remaining", description=f"{charges:,}"), ephemeral=True
+        )
 
 
 async def setup(bot):
