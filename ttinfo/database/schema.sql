712039cfbaae92e55840740696a55973d9bfdfa7
Matthew Brough <matthew.brough@hotmail.com>
2023-11-08 17:37:17 +0000

    initial impl

--- ttinfo/database/schema.sql
+++ ttinfo/database/schema.sql
@@ -4,9 +4,17 @@ EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;
 
+DO $$ BEGIN
+    CREATE TYPE COORDS AS (x FLOAT, y FLOAT, z FLOAT, h FLOAT);
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
 CREATE TABLE IF NOT EXISTS keys(vrp_id INT, server SERVER, private TEXT, public TEXT, PRIMARY KEY(vrp_id, server));
+CREATE TABLE IF NOT EXISTS donations(vrp_id INT, server SERVER, quantity INT, amount_used INT DEFAULT 0, reccuring BOOLEAN DEFAULT FALSE, PRIMARY KEY(vrp_id, server));
 CREATE TABLE IF NOT EXISTS snowflake2user(vrp_id INT PRIMARY KEY, snowflake BIGINT NOT NULL);
 CREATE TABLE IF NOT EXISTS wealth(vrp_id INT, server SERVER, wallet BIGINT, bank BIGINT, loan BIGINT, PRIMARY KEY(vrp_id, server));
 CREATE TABLE IF NOT EXISTS item_info(item_id TEXT PRIMARY KEY, description TEXT, name TEXT, weight FLOAT);
 CREATE TABLE IF NOT EXISTS aliases(vrp_id INT NOT NULL, name TEXT NOT NUll, last_seen TIMESTAMPTZ, PRIMARY KEY(vrp_id, name));
 CREATE TABLE IF NOT EXISTS playtime(vrp_id INT, server SERVER, playtime INTERVAL, job TEXT, date_seen DATE, PRIMARY KEY(vrp_id, server, job, date_seen));
+CREATE TABLE IF NOT EXISTS positions(vrp_id INT NOT NUll, server SERVER, position COORDS NOT NULL)
--- ttinfo/extensions/tasks/positions.py
+++ ttinfo/extensions/tasks/positions.py
@@ -12,12 +12,14 @@ from ...core.utils.errors import TTInfoException
 
 if TYPE_CHECKING:
     from ttinfo.core.bot import Bot
+    from ...http.models import Player, Coords
 
 
 class Tasks(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         self.logger = self.bot.log_handler.getChild(self.qualified_name)
+        self.positions: dict[Player, Coords] = {}
 
     async def cog_load(self) -> None:
         self.logger.info(f"{self.qualified_name} cog loaded")
@@ -33,7 +35,16 @@ class Tasks(commands.Cog):
     async def task(self):
         for server in Server:
             request = await self.bot.tycoon_client.fetch_positions(server, self.bot.tycoon_client.fallback_key)
-            print(request[0].player.name, request[0].position)
+            for position in request:
+                if self.positions.get(position.player, None) == position.position:
+                    continue
+                self.positions[position.player] = position.position
+                await self.bot.pool.execute(
+                    "INSERT INTO positions(vrp_id, server, position) VALUES($1, $2, $3)",
+                    position.player.vrp_id,
+                    server.name,
+                    (position.position.x, position.position.y, position.position.z, position.position.h),
+                )
 
     @task.before_loop
     async def wait_for_ready(self):
