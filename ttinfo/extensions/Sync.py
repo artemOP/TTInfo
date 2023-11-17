69bc84daa366afce1333c74e5b9a6ee78a1626df
matthew <matthew.brough@hotmail.com>
2023-11-17 13:59:11 +0000

    Sync, now with hybrid
    needs an error handler for the text command checks

--- ttinfo/extensions/Sync.py
+++ ttinfo/extensions/Sync.py
@@ -1,12 +1,22 @@
 from __future__ import annotations
 
+from enum import Enum
 from typing import TYPE_CHECKING, Literal
 
+import discord
+from discord import app_commands
 from discord.ext import commands
 
 if TYPE_CHECKING:
     from ttinfo.core.bot import Bot
     from discord.ext.commands import Context
+
+
+class SyncOptions(Enum):
+    local_add = "+"
+    local_remove = "-"
+    global_add = "*"
+    global_remove = "/"
 
 
 class Sync(commands.Cog):
@@ -20,44 +30,41 @@ class Sync(commands.Cog):
     async def cog_unload(self) -> None:
         self.logger.info(f"{self.qualified_name} cog unloaded")
 
-    @commands.command(name="sync", description="Sync command")
+    @commands.hybrid_command(name="sync", description="Sync command")
     @commands.is_owner()
-    async def sync(self, ctx: Context, option: Literal["*", "/", "+", "-"] = "*"):
+    @app_commands.default_permissions()
+    @commands.guild_only()
+    async def sync(self, ctx: Context, option: SyncOptions = SyncOptions.global_add):
         """Sync commands with set options
 
         :param ctx: Context
-        :param option:
-            *: Global Sync
-            /: Clear Global Commands
-            +: Local Sync
-            -: Clear Local Sync
+        :param option: Sync option
         """
-        async with ctx.typing(ephemeral=True):
-            await ctx.message.delete(delay=10)
-            match option:
-                case "*":
-                    await self.bot.tree.sync(guild=None)
-                    response_message = "Commands synced globally"
-                case "/":
-                    self.bot.tree.clear_commands(guild=None)
-                    await self.bot.tree.sync(guild=None)
-                    response_message = "Global commands removed"
+        await ctx.message.delete(delay=10)
+        match option:
+            case SyncOptions.global_add:
+                await self.bot.tree.sync(guild=None)
+                response_message = "Commands synced globally"
+            case SyncOptions.global_remove:
+                self.bot.tree.clear_commands(guild=None)
+                await self.bot.tree.sync(guild=None)
+                response_message = "Global commands removed"
 
-                case "+":
-                    self.bot.tree.copy_global_to(guild=ctx.guild)
-                    await self.bot.tree.sync(guild=ctx.guild)
-                    response_message = "Commands synced locally"
+            case SyncOptions.local_add:
+                self.bot.tree.copy_global_to(guild=ctx.guild)
+                await self.bot.tree.sync(guild=ctx.guild)
+                response_message = "Commands synced locally"
 
-                case "-":
-                    self.bot.tree.clear_commands(guild=ctx.guild)
-                    await self.bot.tree.sync(guild=ctx.guild)
-                    response_message = "Local commands removed"
+            case SyncOptions.local_remove:
+                self.bot.tree.clear_commands(guild=ctx.guild)
+                await self.bot.tree.sync(guild=ctx.guild)
+                response_message = "Local commands removed"
 
-                case _:
-                    response_message = "Invalid option passed"
-            # await self.bot.prepare_mentions.start()
-            return await ctx.send(response_message, delete_after=10)
+            case _:
+                response_message = "Invalid option passed"
+        return await ctx.send(response_message, delete_after=10)
 
 
-async def setup(bot):
-    await bot.add_cog(Sync(bot))
+async def setup(bot: Bot):
+    guild = bot.env_values["discord_guild"]
+    await bot.add_cog(Sync(bot), guild=discord.Object(id=guild))
