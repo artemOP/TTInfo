ef4789c7eb3de741a138cbeac3e05c16d0250a82
matthew <matthew.brough@hotmail.com>
2024-03-01 16:00:38 +0000

    send codeblock on request for copy pasting

--- ttinfo/extensions/timestamp/timestamp.py
+++ ttinfo/extensions/timestamp/timestamp.py
@@ -10,6 +10,7 @@ from discord.ext import commands
 from discord.interactions import Interaction
 
 from .transformers import Timezone, Format
+from ...core.utils import to_codeblock
 
 TimestampStyle = Literal["f", "F", "d", "D", "t", "T", "R"]
 if TYPE_CHECKING:
@@ -33,27 +34,32 @@ class Timestamp(commands.Cog):
     async def to_timestamp(
         self,
         interaction: Interaction[Bot],
-        format: Transform[str, Format],
         year: Range[int, 1970, 2100] | None = None,
         month: Range[int, 1, 12] | None = None,
         day: Range[int, 1, 31] | None = None,
         hour: Range[int, 0, 23] | None = None,
         minute: Range[int, 0, 59] = 0,
         timezone: Transform[ZoneInfo, Timezone] | None = None,
+        format: Transform[str, Format] = "",
+        raw: bool = False,
     ):
         """Generate a discord timestamp for a given time
 
         Args:
-            interaction (Interaction[Bot]): _description_
-            year (Range[int, 1970, 2100] | None, optional): The year
-            month (Range[int, 1, 12] | None, optional): The month
-            day (Range[int, 1, 31] | None, optional): The day
-            hour (Range[int, 0, 23] | None, optional): The hour
-            minute (Range[int, 0, 59] | None, optional): The mintue
-            timezone (str | None, optional): The timezone
+            interaction (Interaction[Bot]): ...
+            year (Range[int, 1970, 2100] | None): The year
+            month (Range[int, 1, 12] | None): The month
+            day (Range[int, 1, 31] | None): The day
+            hour (Range[int, 0, 23] | None): The hour
+            minute (Range[int, 0, 59] | None): The mintue
+            timezone (str | None): The timezone
             format (TimestampStyle | None): The format to return the discord timestamp in
+            raw (bool): Set True to return as a copyable code block
         """
-        await interaction.followup.send(format, ephemeral=True)
+        if raw:
+            await interaction.followup.send(to_codeblock(format, language="MD", escape_md=False))
+        else:
+            await interaction.followup.send(format, ephemeral=True)
 
 
 async def setup(bot):
